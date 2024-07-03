ed6e406ca7c8cc3176314d3dead851e1285eb3fb
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-07-03 15:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+//your JS code here. If required.  
+let cookies = document.cookie.split("; ").map((ele) => {
+      let [key, ...value] = ele.split("=");
+      return [key, value.join("=")];
+    });
+let checkFontSize = cookies.find((ele)=>ele[0]=="fontsize")
+let checkFontColor=cookies.find((ele)=>ele[0]=="fontcolor")
+
+if(checkFontColor && checkFontSize){
+	setAcc(checkFontSize[1],checkFontColor[1])
+}
+function savePreferences(){
+	let fontsize =document.getElementById("fontsize").value
+	let fontcolor = document.getElementById("fontcolor").value
+	document.cookie=`fontsize=${fontsize}`
+	document.cookie = `fontcolor=${fontcolor}`
+	setAcc(fontsize,fontcolor)
+}
+function setAcc(size,color){
+	document.getElementById("fontsize").value = size
+	document.getElementById("fontcolor").value= color
+	document.documentElement.style.setProperty('--fontsize',size);
+    document.documentElement.style.setProperty('--fontcolor',color);
+	
+}
